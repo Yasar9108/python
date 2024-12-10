f2ee3e827d1df04699f168d5167a597ca3237ed1
Yasar9108 <1EP21IS121.yasarr@gmail.com>
2024-12-11 02:28:02 +0530

    click here

---
+++ insta_post_update.py
@@ -0,0 +1,4 @@
+from instabot import bot
+bot =bot()
+bot.login(username="Enter your insta id name",password="Enter your password")
+bot.upload_photo("pic.jpg",caption="Enter Your caption for this post")
